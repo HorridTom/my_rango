c56f18f2c8a4aa56380abf8769a584ce2f4c249a
HorridTom <woodcock.thomas@gmail.com>
2016-05-18 22:13:41 +0100

    Added "about" view and associated url mapping.

--- twoodcock_tango_with_django_project/rango/views.py
+++ twoodcock_tango_with_django_project/rango/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 # It takes a HttpResponse "request"
 # and returns another HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("""Rango says: Hey there partner! <br/>
+    <a href='/rango/about'>About</a>""")
+
+
+# The "about" view
+def about(request):
+    return HttpResponse("""Rango says here is the about page. <br/>
+    <a href='/rango'>Back to index<a/>""")
