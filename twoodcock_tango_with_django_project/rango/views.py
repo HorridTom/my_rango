8f4fb47336b88d71f0790cf7fc78c7f1794d2ab6
HorridTom <woodcock.thomas@gmail.com>
2016-05-18 23:06:17 +0100

    Added my first template and modified the index view to render it.

--- twoodcock_tango_with_django_project/rango/views.py
+++ twoodcock_tango_with_django_project/rango/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # My first view - called "index"
 # It takes a HttpResponse "request"
 # and returns another HttpResponse
 def index(request):
-    return HttpResponse("""Rango says: Hey there partner! <br/>
-    <a href='/rango/about'>About</a>""")
+    # Construct a dictionary to pass the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the clinet.
+    # We use the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 # The "about" view
