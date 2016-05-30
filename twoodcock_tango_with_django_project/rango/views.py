f9ca936bbefefa291cebe3008f08d089dcb0f17c
HorridTom <woodcock.thomas@gmail.com>
2016-05-30 10:32:17 +0100

    Added template for the about page.

--- twoodcock_tango_with_django_project/rango/views.py
+++ twoodcock_tango_with_django_project/rango/views.py
@@ -9,7 +9,7 @@ def index(request):
     # Construct a dictionary to pass the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # Return a rendered response to send to the clinet.
+    # Return a rendered response to send to the client.
     # We use the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
@@ -17,5 +17,7 @@ def index(request):
 
 # The "about" view
 def about(request):
-    return HttpResponse("""Rango says here is the about page. <br/>
-    <a href='/rango'>Back to index<a/>""")
+    # Construct dictionary to pass variable name to the template
+    context_dict = {'name': "Tom"}
+    # Returna rendered response
+    return render(request, 'rango/about.html', context=context_dict)
